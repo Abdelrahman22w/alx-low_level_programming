5a0720e7e822c083c94b0e1895e4e8dcac028f51
Abdelrahman22w <abdelrahmanwaheed22w@gmaiul.com>
2023-03-25 14:47:00 -0700

    fsdfsdf

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,4 @@
 #include "main.h"
-* print_times_table prints time table of n
-* @n: takes number input
-*/
 void print_times_table(int n)
 {
 int prod, mult, num;
@@ -16,17 +13,18 @@ _putchar(',');
 _putchar(' ');
 prod = num * mult;
 if (prod <= 9)
-_putchar(' )';
+_putchar(' ');
 if (prod <= 99)
 _putchar(' ');
 if (prod >= 100)
 {
-_putchar ((prod / 100) + 48);
-_putchar ((prod / 10) % 10 + 48);
- else if (prod <= 99 && prod >= 10)
-{ _putchar((prod / 10) + 48);
+_putchar((prod / 100) + 48);
+_putchar((prod / 10) % 10 + 48);
+} else if (prod <= 99 && prod >= 10)
+_putchar ((prod / 10) + 48);
 _putchar ((prod % 10) + 48);
 }
-_putchar('\n');
+	_putchar('\n');
+}
 }
 }
