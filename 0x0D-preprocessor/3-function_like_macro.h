bb81240f5941df34c6ab317ff5d338bbf34032be
Abdelrahman22w <abdelrahmanwaheed22w@gmaiul.com>
2023-04-17 07:35:15 -0700

    My first commit

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * if x less than 0 (negative) retun -x else return x
+ */
+#define ABS(x) ((x)<0 ? (-x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * define a new method that computes the sum of x, y
+ */
+#define SUM(x, y) ((x)+(y))
+
+#endif
