0fa348dbcb1642072b827cfea0d248a8ccd7e1cc
Abdelrahman22w <abdelrahmanwaheed22w@gmaiul.com>
2023-04-17 07:49:37 -0700

    My first commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main -  prints the name of the file it was compiled from
- * RETURN: 0
+ * main - prints the name of the file it was compiled from.
+ *
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
