579653a5fb3dcf4b81dee25412885ab8a39a2751
Abdelrahman22w <abdelrahmanwaheed22w@gmaiul.com>
2023-04-17 07:42:23 -0700

    My first commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,10 @@
 #ifndef MACRO_H
 #define MACRO_H
 /**
- * if x less than 0 (negative) retun -x else return x
+ * if @x: less than 0 (negative) retun -x else return x
+ * @x: input integer or float
+ * @ABS(x): return the positive value of x
  */
-#define ABS(x) ((x)<0 ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
