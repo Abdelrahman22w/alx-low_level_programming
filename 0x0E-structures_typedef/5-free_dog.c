13c70716313b0161879c1b8433e631d91ed32436
Abdelrahman22w <abdelrahmanwaheed22w@gmaiul.com>
2023-04-17 08:40:45 -0700

    My first commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - a function to free the stored values of dog
+ * @d: a pointer to dog_t
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d);
+		free(d->name);
+		free(d->owner);
+	}
+
+}
