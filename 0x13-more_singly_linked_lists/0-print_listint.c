1915fafcb18da6c1ba3d4cd3a84ed138409974c9
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-04-29 05:17:18 -0700

    My first commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
  * print_listint - prints all the elements of a linked list
  * @h: pointer to the list (head)
  * Return: number of nodes printed
-**/
+*/
 size_t print_listint(const listint_t *h)
 {
 	size_t n = 0;
