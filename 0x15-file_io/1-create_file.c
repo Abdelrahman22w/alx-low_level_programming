175f6949e926b40e8e35fd4d2168460babccef58
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-07-10 19:29:10 +0300

    second commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * create_file - a function to create a file
- * @filename: the name of the file
- * @text_content: the char of the file
- * Return: 1 if succeeded, -1 if failed
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -12,18 +14,25 @@ int create_file(const char *filename, char *text_content)
 	int rwr;
 
 	if (!filename)
-		retrun(-1);
-	fd = open(filename, O_CREATE | O_WRONLY | O_TRUNC, 0600)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
+
 	if (!text_content)
 		text_content = "";
+
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
+
 	rwr = write(fd, text_content, nletters);
+
 	if (rwr == -1)
 		return (-1);
+
 	close(fd);
+
 	return (1);
 }
